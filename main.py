9ade35b69e8cf122499296636455e35a10f141fc
Rasmus <r.serrestam@gmail.com>
2023-01-12 12:45:24 +0100

    ja

--- main.py
+++ main.py
@@ -2,18 +2,32 @@ import random
 deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]*4
 random.shuffle(deck)
 players = []
-print(deck)
-
-amountOfPlayers = input("Enter amount of players(Max 4): ")
+money = []
+activeBet = []
+run = True
+round = 0
+amountOfPlayers = 2#input("Enter amount of players(Max 4): ")
 
 for i in range(int(amountOfPlayers)):
     players.append([])
+    money.append(1000)
+    activeBet.append(0)
 players.append([])
 
-for i in range(2):
+def giveCards():
     for j in range(len(players)):
         players[j].append(deck[-1])
         deck.insert(0, deck[-1])
         deck.pop(-1)
 
-print(players)
+while run:
+    round += 1
+    print(f"Runda {round}")
+    # Fråga spelarna hur mycket dom vill beta
+    for i in range(len(players)):
+        input("Hur mycket vill spelare "+ str(i+1) +" betta? ")
+    break
+
+
+
+print(players, money, activeBet)
